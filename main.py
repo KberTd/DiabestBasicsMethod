3734af18f29e167f1833058ea90f77a7c4c4966c
Arda GÖLGE <92367739+KberTd@users.noreply.github.com>
2021-12-28 01:34:53 +0300

    V 0.2.0

--- main.py
+++ main.py
@@ -1,66 +1,32 @@
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn import svm
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from sklearn.neural_network import MLPClassifier
+import pandas as pd #pandas veri işlemesi ve analiz için ihtiyaç kullanılan yazılım kütüphanesidir.
+from sklearn.model_selection import train_test_split #sklearn machine learning için kullanılıyor
+from sklearn.impute import SimpleImputer #datayı satır ve sutün olarak dökmek için kullanılıyor
+from sklearn import metrics #metrics sklearn kütüphanesinde bir classtır. accuracy_score fonksiyonu Classifierin accuracysini hesaplar
+from sklearn.ensemble import RandomForestClassifier #hızlı basit ve esnek olduğu için bunu kullanıyoruz
 
 
-diabetes = pd.read_csv("C:\\Users\\ARDA\\PycharmProjects\\pythonProjectybs.py\\diabetes.csv")
+df = pd.read_csv("C:\\Users\\ARDA\\PycharmProjects\\pythonProjectLastDance\\main.py")
 
 
-X = diabetes.drop("Outcome", axis = 1) # axis = 0 : satır //// axis = 1 : sütun
-y = diabetes["Outcome"]
+X = df.drop('Outcome', axis=1) #değişkenlerin drop üzerinde görüntülenmesi.  axis  x 1 axis y 0
+y = df['Outcome'] #OUTCOME ÇIKTISI OLARAK AYIRMA X VE Y AXİS= 1 SÜTUNDUR AXİS= 0 SATIRDIR
 
-#
-# print (X)
-# print(y)
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.33,
+                                                    random_state=7)
+#veri seti öncelikle train ve test olarak ikiye ayrılıyor.
+#x = bağımsız değişken y = hedef(target) değişken test size = 0.33 test için ayrılan küme yüzdesi
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
+fill_values = SimpleImputer(missing_values=0, strategy='mean') #eksik değerler için kullandık
+X_train = fill_values.fit_transform(X_train)
+X_test = fill_values.fit_transform(X_test)
+#satır sonucu elde etmek için her modelden yapılan tahminler birleştirilir.
+# ( normalde 2 değerden sadece 1 tanesi fit lenir 2 değerin fitlenmesi problemlere yol açabilir ama bizim uygulamamızda probleme yol açmadı.)
 
-sc= StandardScaler() # nested array dönüyor.
-                     # z = (x - u) / s
-                     # z = new value after calculation
-                     # x = old value
-                     # u = mean
-                     # s = standard deviation
-#
-# print(X_train)
-# print (X_train)
-X_train = sc.fit_transform(X_train) # nested array dönüyor. fit_transform does the math to fit all the values
-                                    # so that all the variables affect the model at the same level.
-# print (X_train)
-
-X_test = sc.transform(X_test) # we just transformed because we didn't want to calculate
-                              # mean and standard deviation again.
-
-#
-# print (X_train)
-
-
-clf = svm.SVC()
-
-clf.fit(X_train, y_train) # X_train ve y_train datalarını al ve SVC modeline uyumlandır.
-pred_clf = clf.predict(X_test)
-print(classification_report(y_test, pred_clf))
-print(confusion_matrix(y_test, pred_clf))
-
-
-
-mlpc = MLPClassifier(hidden_layer_sizes=(11,11,11), max_iter=100)
-                    #3 hidden layers and size of each are 11
-                    #maximum iterations is 500. It means, it will go through the data 500 times.
-
-mlpc.fit(X_train,y_train)
-pred_mlpc = mlpc.predict(X_test)
-
-# print (classification_report(y_test, pred_mlpc))
-# print(confusion_matrix(y_test, pred_mlpc))
-
-a = [[6,148,72,35,0,33.6,0.627,50]]
-
-Xnew = sc.transform(a)
-ynew1 = clf.predict(Xnew)
-ynew2 = mlpc.predict(Xnew)
-print(ynew1, ynew2)
+rfc = RandomForestClassifier()
+rfc.fit(X_train, y_train)
+predictions = rfc.predict(X_test)
+print(df)
+print("Random Forest Accuracy_Score =", format(metrics.accuracy_score(y_test, predictions)))
+#rfc.fit  x train ve y traini rfc ye uyumlandırıyor.
+# Eğer etiketleri değil de olasılıkları öğrenmek istiyorsak predict_proba fonksiyonunu kullanmamız gerekiyor.
+# Sonuçlar her etikete ait olma yüzdesini içeriyor.
